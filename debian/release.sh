e0120bb1c117f3f48815bfe9d895f3498f8a1f94
David Lechner <david@lechnology.com>
2019-06-25 23:55:27 -0500

    backport to ev3dev-stretch

--- debian/release.sh
+++ debian/release.sh
@@ -6,11 +6,24 @@ set -e
 
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
+distribution=$(dpkg-parsechangelog -S Distribution)
+codename=$(debian-distro-info --codename --${distribution})
 
-debuild -S -d
+export DEB_BUILD_OPTIONS=nocheck # tests fail because of pbuilder
 
-dput ppa:ev3dev/tools ../${source}_${version}_source.changes
+OS=debian DIST=${codename} ARCH=amd64 pbuilder-ev3dev build
+OS=debian DIST=${codename} ARCH=armhf PBUILDER_OPTIONS="--binary-arch" pbuilder-ev3dev build
+OS=debian DIST=${codename} ARCH=armel PBUILDER_OPTIONS="--binary-arch" pbuilder-ev3dev build
+OS=raspbian DIST=${codename} ARCH=armhf pbuilder-ev3dev build
 
-debuild -d -- clean
+debsign ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
+debsign ~/pbuilder-ev3dev/debian/${codename}-armhf/${source}_${version}_armhf.changes
+debsign ~/pbuilder-ev3dev/debian/${codename}-armel/${source}_${version}_armel.changes
+debsign ~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}_armhf.changes
+
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-armhf/${source}_${version}_armhf.changes
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-armel/${source}_${version}_armel.changes
+dput ev3dev-raspbian ~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}_armhf.changes
 
 gbp buildpackage --git-tag-only
